63653168d6002012ea0fb282266b67533218a830
F. Talha Altınel <talhaaltinel@hotmail.com>
2020-05-28 23:54:55 +0100

    favicon and introduction

--- components/introduction.js
+++ components/introduction.js
@@ -1,10 +1,15 @@
 export default function Introduction() {
     return (
-      <section className="hero is-info is-large">
+      <section className="hero is-info is-large" style={{marginTop: 80}}>
         <div className="hero-body">
-          <div className="container">
-            <h1 className="title"> Large title </h1>
-            <h2 className="subtitle"> Large subtitle </h2>
+          <div class="columns">
+            <div className="column is-4 is-offset-2">
+              <h1 className="title"> Large title Long Title Goes Down The Doodle </h1>
+              <h2 className="subtitle"> Large subtitle </h2>
+            </div>
+            <div className="column is-4">
+              <img src="/web_development.png" />
+            </div>
           </div>
         </div>
       </section>
--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,10 @@ export default function HomePage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Goldenhand Software</title>
 
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <Navigation />
