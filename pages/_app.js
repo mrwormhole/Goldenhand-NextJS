9297d50269839c27ba590edf43551a1dcf1c12b6
F. Talha Altınel <talhaaltinel@hotmail.com>
2020-07-28 15:17:55 +0100

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 
-import "../styles/_app.scss"
-
+import "../styles/_app.scss";
 
 export default function App({Component, pageProps}) {
     return <Component {...pageProps} />
